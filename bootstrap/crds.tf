b37fea5a52507a51fbfd89e81cc4cc934c9adb8c
kors-dev <korsuna.dev@gmail.com>
2025-09-07 17:01:14 +0000

    fix(kubeconfig): update certificate authority data and server address refactor(crds.tf): clarify comments and ensure resource dependency on cluster

--- bootstrap/crds.tf
+++ bootstrap/crds.tf
@@ -11,8 +11,10 @@ data "kubectl_file_documents" "gateway_api" {
   content = data.http.gateway_api_crds.response_body
 }
 
-# Застосовуємо КОЖЕН документ окремим ресурсом
+# Ставимо КОЖЕН документ. Чекаємо кластер!
 resource "kubectl_manifest" "gateway_api_crds" {
   for_each  = { for i, doc in data.kubectl_file_documents.gateway_api.documents : i => doc }
   yaml_body = each.value
+
+  depends_on = [kind_cluster.this]  # <-- критично
 }
